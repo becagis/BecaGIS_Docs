2625c8ea49b3151e1fa9552cf2babc043e7eb865
Trương Thanh Tùng <ttungbmt@gmail.com>
2023-03-28 16:08:43 +0700

    Update changes

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -7,10 +7,8 @@ git fetch --all
 
 git checkout --force "origin/main"
 
-export APP_VERSION=latest
+docker compose --env-file ./.env.deploy -f docker-compose.deploy.yml pull
 
-docker compose -f docker-compose.deploy.yml pull
-
-docker compose -f docker-compose.deploy.yml up -d --force-recreate
+docker compose --env-file ./.env.deploy -f docker-compose.deploy.yml up -d --force-recreate
 
 docker image ls --filter "reference=becagis/docs" --filter "dangling=true" -q | xargs --no-run-if-empty docker rmi
